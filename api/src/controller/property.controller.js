23ea55c81258b3e3c4dd9658351c4bf993028d9d
quchallenge2021 <quchallenge2021@gmail.com>
2023-02-21 21:44:38 -0300

    include

--- api/src/controller/property.controller.js
+++ api/src/controller/property.controller.js
@@ -1,8 +1,16 @@
-const {property} = require('../../models');
+const {property,zone,propertyType,owner} = require('../../models');
 
 const { all, paginate, create, findOne, update, destroy } = require('../Generic/FactoryGeneric');
 
-exports.GetAll = all(property);
+exports.GetAll = all(property,
+    {
+        inclued : 
+        [ 
+            { model: zone, attributes : ['name']  }, 
+            { model: propertyType, attributes:['description']}, 
+            { model: owner, attributes:['fullName','phone','email']}
+        ]
+    });
 exports.Paginate = paginate(property);
 exports.Create = create(property, ['ZoneId','PropertyTypeId','OwnerId','street','number','floor','dept','isFor','status','description']);
 exports.GetById = findOne(property);
