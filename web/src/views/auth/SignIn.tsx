23366c677ba139d47431f255ea66e76c906ee4e7
atomotechnology <atomotechnology@gmail.com>
2023-04-10 00:10:50 -0300

    REFACTORING PART 02

--- web/src/views/auth/SignIn.tsx
+++ web/src/views/auth/SignIn.tsx
@@ -1,14 +1,14 @@
-import { useContext } from 'react';
-import { NavLink } from 'react-router-dom';
-import { useState } from 'react';
-
-
-import http from './../../api/axios';
-import { useForm } from '../../hooks/useForm';
-import { AuthContext } from '../../context/authContext';
-import Box from '../../components/Box';
-import CustomInput from '../../components/CustomInput';
-import FormError from '../../components/FormError';
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import http from "./../../api/axios";
+import { useForm } from "../../hooks/useForm";
+import { AuthContext } from "../../context/authContext";
+import Box from "../../components/Box";
+import CustomInput from "../../components/CustomInput";
+import FormError from "../../components/FormError";
+import InlineDots from "../../components/loadings/Inlinedots";
+import { validateForm } from "../../helpers/form";
 
 const SignIn = () => {
 
@@ -17,88 +17,94 @@ const SignIn = () => {
   const [loginError, setLoginError] = useState<string | null>(null);
 
   const { signIn } = useContext(AuthContext);
-  const { values, handleInputChange, email, password, reset } = useForm({ email: '', password: '' });
+  const { values, handleInputChange, email, password, reset } = useForm({
+    email: "",
+    password: "",
+  });
 
-  const verifyForm = () => {
-    let ok = true;
-    let error: any = {};
-    if (!email.trim().length) {
-      ok = false;
-      error.email = true;
-    }
-    if (!password) {
-      ok = false;
-      error.password = true;
-    }
-    setErrors(error);
-    return ok;
-  };
 
   const handleSubmitLogin = async (e: any) => {
     e.preventDefault();
-    if (verifyForm()) {
-      setLoading(true);
-      try {
-        const r = await http.post('/auth/signin', values);
-        if (r.data.ok) {
-          signIn(r.data);
-          reset();
-          setLoading(false);
-        } else {
-          setLoginError(r.data.message);
-        }
-      } catch (error: any) {
-        console.log(error)
-        if (error.response) {
-          setLoginError(error.response.data.message);
-          setTimeout(() => {
-            setLoginError(null);
-          }, 5000);
-        }
-      } finally {
+    const { error, ok } = validateForm({ ...values })
+    setErrors(error)
+    if (!ok) return false
+    setLoading(true);
+    try {
+      const r = await http.post("/auth/signin", values);
+      if (r.data.ok) {
+        signIn(r.data);
+        reset();
         setLoading(false);
+      } else {
+        setLoginError(r.data.message);
       }
+    } catch (error: any) {
+      if (error.response) {
+        setLoginError(error.response.data.message);
+        setTimeout(() => {
+          setLoginError(null);
+        }, 8000);
+      }
+    } finally {
+      setLoading(false);
     }
+
   };
 
   return (
-    <div className='flex h-screen w-screen dark:bg-gray-900 items-center justify-center'>
+    <div className="flex h-screen w-screen dark:bg-gray-900 items-center justify-center">
+      <Box className=" w-full max-w-[420px] mx-3 sm:mx-0 sm:w-96 ">
+        <form
+          onSubmit={handleSubmitLogin}
+          className="flex items-center justify-between flex-col "
+        >
+          <h3 className="title-form self-start mb-4 !text-xl sm:!text-3xl">Inicia sesión</h3>
 
-      <Box className=' w-full max-w-[420px] mx-3 sm:mx-0 sm:w-80 '>
+          {loginError && <FormError text={loginError} />}
 
-        <form onSubmit={handleSubmitLogin} className='flex items-center justify-between flex-col '>
+          <CustomInput
+            type="email"
+            placeholder="example@gmail.com"
+            onChange={(val) => handleInputChange(val, "email")}
+            hasError={errors?.email}
+            required
+            label="Email"
+            errorText="Email obligatorio y válido."
+          />
 
-          <h3 className='title-form self-start mb-4 !text-xl sm:!text-3xl'>Inicia sesión</h3>
-
-          {loginError && (<FormError text={loginError} />)}
+          <CustomInput
+            type="password"
+            placeholder=".lk8Tx9W/"
+            onChange={(val) => handleInputChange(val, "password")}
+            required
+            label="Contraseña"
+            hasError={errors?.password}
+            errorText="Contraseña obligatoria ."
+          />
 
           <fieldset>
-            <label> Email</label>
-            <CustomInput type='email' placeholder='example@gmail.com' onChange={(val) => handleInputChange(val, 'email')} />
-            {errors?.email && <FormError text='Email obligatorio y válido.' />}
+            <button disabled={loading} className="btn gradient" type="submit">
+              {loading ? (
+                <div className="flex items-center gap-4">
+                  <span>Espere</span>
+                  <InlineDots />
+                </div>
+              ) : (
+                <span>Inicia sesión</span>
+              )}
+            </button>
           </fieldset>
 
-          <fieldset>
-            <label>Contraseña</label>
-            <CustomInput type='password' placeholder='.lk8Tx9W/' onChange={(val) => handleInputChange(val, 'password')} />
-            {errors?.password && <FormError text='Contraseña obligatoria .' />}
-          </fieldset>
-
-          <fieldset>
-            <button disabled={loading} className='btn gradient' type='submit'>{!loading ? ('Inicia sesión') : 'Espere...'}</button>
-          </fieldset>
-
-          <div className='register-section text-sx my-2'>
-            <span className='text-sm'>
-              <NavLink
-                to='/forget-password'
-                className='text-pink-700 dark:text-slate-500 ml-1 hover:underline  p-1'
+          <div className="register-section text-sx my-2">
+            <span className="text-sm">
+              <Link
+                to="/forget-password"
+                className="text-pink-700 dark:text-slate-500 ml-1 hover:underline  p-1"
               >
                 ¿Olvidaste tu contraseña ?
-              </NavLink>
+              </Link>
             </span>
           </div>
-
         </form>
       </Box>
     </div>
